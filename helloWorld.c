c86ea5f9fa3195d2298291251d57b995b82e8843
LOKESHKARMOKAR <146501836+LOKESHKARMOKAR@users.noreply.github.com>
2024-07-30 23:20:13 +0530

    first commit

---
+++ helloWorld.c
@@ -0,0 +1,109 @@
+#include<stdio.h>
+int main(){
+	//printf("Hello World");
+	 
+	 	// 1. variable decleriaction
+		// 		int st_roll = 101; declear of variable and initialize the value
+		// 		int roll ;  declear of variable
+		// 2. variable initalization
+		// 3.  mutability of value
+		// 4. diff type of value stroring
+		/*
+		int roll = 101;
+		roll = 20;
+		roll = 30;
+		printf("%d",roll);
+		*/
+		
+		// int a = 10;
+		// float b = 10.5;
+		// double db = 10.56897;
+		// char c = 'L';
+		// long int phone = 9382291059;
+		// short int si = 12;
+		
+		// printf("The value of a : %d\n", a);
+		// printf("The value of b : %.1f\n", b);
+		// printf("The value of db : %lf\n", db);
+		// printf("The value of c : %c\n", c);
+		// printf("The value of phone : %ld\n", phone);
+		// printf("The value of si : %d\n", si);
+
+
+        // user input
+
+       
+        // float a ;
+        // printf("Enter the Float value : \n");
+        // scanf("%f",&a);
+        // printf("\nvalue of a is : %.1f", a);
+        // int a =10;
+        // int b=15;
+        // printf("sum of two number is :%d", a+b);
+
+        // int a ;
+        // int b;
+        // int result;
+        // printf("Enter the value of a :\n");
+        // scanf("%d",&a);
+        // printf("Enter tthe value of b :\n");
+        // scanf("%d",&b);
+        // result = a%b;
+        // printf("Sum of two number is : %d", result);
+        // printf("%d+%d=%d", a,b,result);
+        // printf("\nThe addition of %d and %d is : %d", a,b,result);
+
+        // int a =100;
+        // int b = 10;
+        // int c = a>=b;
+        // printf("%d", c);
+
+        // int a =10;
+        // int b=20;
+        // b -= a;
+        // printf("%d", b);
+
+        // // logical and
+
+        // int a = 20;
+        // int b =10;
+        // int result = (a>b)&&(a==b);
+        // printf("%d", result);
+
+        // logical or
+
+        // int a = 20;
+        // int b =10;
+        // int result = (a>b)||(a==b);
+        // printf("%d", result);
+
+        // logical not
+
+        // int a = 5;
+        // int b = 10;
+        // int result = !(a>b);
+        // printf("%d", result);
+
+        // unary operator
+
+
+        // int a = 10;
+        // a++;
+        // printf("%d", a);
+        int a = 10;
+        int b =20;
+        
+        a--;
+        printf("%d\n", a);
+       
+
+
+		
+	
+	
+
+	
+return 0;
+}
+
+
