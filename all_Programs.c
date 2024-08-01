82385e607bb29a0db2af907bac45ac9b3fa9bddb
LOKESHKARMOKAR <146501836+LOKESHKARMOKAR@users.noreply.github.com>
2024-08-01 08:00:29 +0530

    enter new

--- all_Programs.c
+++ all_Programs.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
 
 // 1. WAP in c to add two number and display the result
@@ -48,10 +49,142 @@ int main(){
     // printf("Circumference : %f\n",c);
     // printf("Area : %f\n",a);
 
-    // 5. 
+    // 5. WAP to enter the temperature into celcius and convert it into a Fahrenheit.
+
+    // float celcius, farenheit;
+    // printf("Enter Temarature : \n");
+    // scanf("%.1f", &celcius);
+    // farenheit = ((celcius*9/5)+32);
+    // printf("Current Temparature %f", farenheit);
+
+    // 6. WAP in c to enter the temparature into farenheit and convert into the celcius
+
+    // float f, c;
+    // printf("Enter Temparature in Farenhite : \n");
+    // scanf("%f", &f);
+    // c= (f-32)*5/9;
+    // printf("Current temparature in farenheit : %f", c);
+
+    // 8. WAP in c enter two number and find out the power value
+// float x,y,z;
+//     printf("Enter the X value : \n");
+//     scanf("%f", &x);
+//      printf("Enter the Y value : \n");
+//     scanf("%f", &y);
+//     z = pow(x,y);
+//     printf("result : %f", z);
+
+    // 7. WAP in c to enter any number by user and find out the square root value
+
+    // float n , r;
+    // printf("Enter the number : \n");
+    // scanf("%f", &n);
+    // r = sqrt(n);
+    // printf("answer : %f", r);
+
+    // 8. WAP in c two enter  two angle and find the third angle
+
+    // int angle1, angle2, angle3;
+    // printf("enter angle 1 : \n");
+    // scanf("%d" , &angle1);
+    // printf("enter angle 2 : \n");
+    // scanf("%d" , &angle2);
+    // angle3 = 180 - (angle1 + angle2);
+    // printf("answer : %d", angle3);
+
+    // 9. WAP in c to calculate area of a Equilateral Triangle 
+
+    // float s,r;
+    // printf("Enter the value of s : ");
+    // scanf("%f", &s);
+    // r = (sqrt(3)/4)*(s*s);
+    // printf("%f", r);
+
+    // 10. WAP in c to enter marks in 5 subject and calculate total, avarage and percentage
+
+    
+    // int bengali, english, math, history, geography, result;
+    // float average, percentage;
+
+    // printf("Enter Bengali Marks: \n");
+    // scanf("%d", &bengali);
+
+    // printf("Enter English Marks: \n");
+    // scanf("%d", &english);
+
+    // printf("Enter Math Marks: \n");
+    // scanf("%d", &math);
+
+    // printf("Enter History Marks: \n");
+    // scanf("%d", &history);
+
+    // printf("Enter Geography Marks: \n");
+    // scanf("%d", &geography);
+
+    // result = bengali + english + math + history + geography;
+    // average = result / 5.0;  // Ensure floating-point division
+    // percentage = (result / 500.0) * 100;
+
+    // printf("The total number of 5 subjects is: %d \n", result); 
+    // printf("The average of 5 subjects is: %.2f \n", average); 
+    // printf("The percentage of 5 subjects is: %.2f \n", percentage); 
+
+    // 11. WAP in c to enter P , T, R and calculate compound interest
+
+    // float p, t, r, si;
+    // printf("Enter the principle amount : ");
+    // scanf("%f", &p);
+    // printf("Enter the Time  : ");
+    // scanf("%f", &t);
+    // printf("Enter the Rate of interest : ");
+    // scanf("%f", &r);
+    // si = (p*t*r)/100;
+    // printf("S.I = %f\n", si);
+    // printf("total amount with interest : %f", (si+p) );
+
+    // 12. WAP in c compound intrest
+
+// float p, t, r, ci;
+//     printf("Enter the principle amount : ");
+//     scanf("%f", &p);
+//     printf("Enter the Time  : ");
+//     scanf("%f", &t);
+//     printf("Enter the Rate of interest : ");
+//     scanf("%f", &r);
+//     ci = p* (pow(1+(r/100),t));
+//     printf("C.I = %f\n", ci);
+//     printf("total amount with interest : %f", (ci+p) );
+
+// 13. wap in c to calculate the area of triangle by using heron's formula
+
+// int a,b,c,s,area;
+// printf("Enter value of a : \n");
+// scanf("%d", &a);
+// printf("Enter value of b : \n");
+// scanf("%d", &b);
+// printf("Enter value of c : \n");
+// scanf("%d", &c);
+// s=(a+b+c)/2;
+// area = sqrt(s* (s-a)* (s-b)* (s-c));
+// printf("area of triangle : %d", area);
+
+// 14. 
+
+// int day ,year, month, week, total;
+// printf("Enter the Days as a integer : ");
+// scanf("%d" , &day);
+// year = day/365;
+// month = day / 30;
+// week =day/7;
+// printf("Number of year : %d\n", year);
+// printf("Number of Month : %d\n", month);
+// printf("Number of Week : %d", week);
+
+
 
 
 
     return 0;
 }
+
 
