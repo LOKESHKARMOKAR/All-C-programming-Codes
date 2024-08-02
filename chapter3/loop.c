5f216623ef3a0a3447c2e76a66c0f6ba05f955dc
LOKESHKARMOKAR <146501836+LOKESHKARMOKAR@users.noreply.github.com>
2024-08-03 00:02:16 +0530

    code

--- chapter3/loop.c
+++ chapter3/loop.c
@@ -24,6 +24,36 @@ int main(){
 //     i++;
 // }while(i<=10);
 
+// #include <stdio.h>
+
+// int main() {
+    // int number, firstDigit, lastDigit, sum;
+
+    // // Prompt the user to enter a number
+    // printf("Enter a number: ");
+    // scanf("%d", &number);
+
+    // // Get the last digit of the number
+    // lastDigit = number % 10;
+
+    // // Find the first digit
+    // firstDigit = number;
+    // while (firstDigit >= 10) {
+    //     firstDigit /= 10;
+    // }
+
+    // // Calculate the sum of the first and last digits
+    // sum = firstDigit + lastDigit;
+
+    // // Display the result
+    // printf("The sum of the first and last digits of %d is: %d\n", number, sum);
+
+//     return 0;
+// }
+
+
+
+
 
 
     return 0;
