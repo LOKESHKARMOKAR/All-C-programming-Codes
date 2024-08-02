c8cd54cd6e52a65c19882ca2e1c71aaf4455f202
LOKESHKARMOKAR <146501836+LOKESHKARMOKAR@users.noreply.github.com>
2024-08-02 23:36:59 +0530

    new code

--- chapter3/loop.c
+++ chapter3/loop.c
@@ -1,11 +1,30 @@
 #include<stdio.h>
 int main(){
 // while loop
-int i = 10;
-while(i>=1){
-    printf("%d\n", i);
-    i--;
-}
+// int i = 10;
+// while(i>=1){
+//     printf("%d\n", i);
+//     i--;
+// }
+
+        // for loop
+
+
+// for(int i =10; i>=1; i--){
+//     printf("%d", i);
+// }
+
+// for(;;){
+//     printf("lokesh");
+// }
+
+// int i=1;
+// do{
+//     printf("%d\n", i);
+//     i++;
+// }while(i<=10);
+
+
 
     return 0;
 }
