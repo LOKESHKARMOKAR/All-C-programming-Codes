051ea7beadab544f3fbf23bd8a66161cfa95226d
LOKESHKARMOKAR <146501836+LOKESHKARMOKAR@users.noreply.github.com>
2024-08-11 16:07:45 +0530

    16th

--- chapter-4/array.c
+++ chapter-4/array.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 int main(){
 
-    int a = 10;
-    int b=20;
-    int C =30;
-    int d = 40;
-    printf("%d",&b);
-    printf("%d", &a);
-    return 0;
-
+    // int a = 10;
+    // int b=20;
+    // int C =30;
+    // int d = 40; 
+    // printf("%d",&b);
+    // printf("%d", &a);
+    // return 0;
+    for(;;){
+        printf("**");
+        
+    }
+return 0;
 }
