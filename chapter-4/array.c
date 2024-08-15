ef5f7e446b6eabdf2909b30a8bb4e96533d236e9
LOKESHKARMOKAR <146501836+LOKESHKARMOKAR@users.noreply.github.com>
2024-08-16 00:55:38 +0530

    23th

--- chapter-4/array.c
+++ chapter-4/array.c
@@ -12,16 +12,101 @@ int main(){
     //     printf("**");
         
     // }
-    int a=10;
-    int b = 20;
-    int c = 30;
-    int d = 40;
-    int e = 50;
-    int f = 60;
-    int g = 70;
+    // int a=10;
+    // int b = 20;
+    // int c = 30;
+    // int d = 40;
+    // int e = 50;
+    // int f = 60;
+    // int g = 70;
     
-    int sum;
-    sum = a+b+c+d+e+f+g;
-    printf("%d", sum);
+    // int sum;
+    // sum = a+b+c+d+e+f+g;
+    // printf("%d", sum);
+
+
+        // ARRAY
+
+    // int rollNo[5]={10,20,30,40,50};
+    // for(int i=0; i<5; i++){
+    //     printf("%d\n" , rollNo[i]);
+    // }
+
+   
+//    int rollNo[5];
+//    printf("Enter the value of N ");
+//    for(int i=0; i<5; i++){
+//     scanf("%d", &rollNo[i]);
+//    }
+//    printf("the value of thr array is ");
+
+//    for(int i=0; i<5; i++){
+//         printf("%d\n" , rollNo[i]);
+//     }
+
+        // *****  read 5 student marks, calculate total and avarage 
+
+//         int marks[5];
+//         int sum=0, avg=0;
+//    printf("Enter the value of N ");
+//    for(int i=0; i<5; i++){
+//     scanf("%d", &marks[i]);
+//    }
+//    printf("the value of thr array is ");
+
+//    for(int i=0; i<5; i++){
+//         sum=sum+marks[i];
+//     }
+//     avg= sum/5;
+//     printf("the sum is %d\n", sum);
+//     printf("avagere is the %d", avg);
+
+
+        //****** */ wap to read an array of 10 integer and count total number of odd number and even number
+
+//         int a[10];
+//         int even=0, odd=0;
+//    printf("Enter the value of N ");
+//    for(int i=0; i<10; i++){
+//     scanf("%d", &a[i]);
+//     if(a[i]%2==0){
+//         even++;
+//     }else{
+//         odd++;
+//     }
+//    }
+//    printf("total number of odd number is %d\n", odd);
+//    printf("total number of even number is %d\n", even);
+
+
+
+        //      
+
+//    int a[5], b[5], c[5];
+
+//     printf("Entwr the items of array A\n ");
+//     for (int i=0; i<5; i++)
+//     {
+//         scanf("%d", &a[i]);
+//     }
+
+//     printf("Entwr the items of array B \n");
+//     for (int i=0; i<5; i++)
+//     {
+//         scanf("%d", &b[i]);
+//     }
+
+//     printf("Entwr the items of array C \n");
+//     for (int i=0; i<5; i++)
+//     {
+//         c[i] = a[i]+b[i];
+//         printf("Sum of array element at index %d is %d\n", i,c[i]);
+//     }
+    
+
+   
+    
+    
+    
 return 0;
 }
