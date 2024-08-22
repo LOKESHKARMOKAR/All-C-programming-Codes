20daada8803aaffe44753bbe0152a9722d80a8a7
LOKESHKARMOKAR <146501836+LOKESHKARMOKAR@users.noreply.github.com>
2024-08-22 23:37:42 +0530

    29th

--- Function/function_01.c
+++ Function/function_01.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
-int main(){
+void sum(){
+    int a, b, sum = 0;
+    printf("Enter two number :\n");
+    scanf("%d %d", &a, &b);
+    sum = a+b;
+    printf("Sum of two number is : %d", sum);
+}
 
+void diff(){
+    int a, b, sum = 0;
+    printf("Enter two number :\n");
+    scanf("%d %d", &a, &b);
+    sum = a-b;
+    printf("diff of two number is : %d", sum);
+}
+int main(){
+sum();
+printf("\n");
+diff();
 
     return 0;
 }
