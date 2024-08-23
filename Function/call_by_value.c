ad1721b1d9de9f8e88555bb3a4fe47cd59e55ded
LOKESHKARMOKAR <146501836+LOKESHKARMOKAR@users.noreply.github.com>
2024-08-23 23:23:42 +0530

    31th

---
+++ Function/call_by_value.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void swap(int *x, int *y){
+int temp = *x;
+*x=*y;
+*y=temp;
+}
+int main(){
+int a=10, b = 20;
+
+printf("Before swapping %d %d\n", a,b);
+swap(&a,&b);
+printf("After swapping %d %d", a,b);
+    return 0;
+}
--- Function/function_01.c
+++ Function/function_01.c
@@ -1,23 +1,20 @@
-#include<stdio.h>
-int sum(int, int);
+// #include <stdio.h>
+// int sum(int, int);
 
-int main(){
-    int a=10,b=20;
-    int r;
-r = sum(10,20); // arguments
+// int main()
+// {
+//     int a = 10, b = 20;
+//     int r;
+//     r = sum(10, 20); // arguments
 
-printf("%d", r);
+//     printf("%d", r);
 
+//     return 0;
+// }
 
+// int sum(int x, int y) // parameter
+// {
 
+//     return (x + y);
+// }
 
-    return 0;
-}
-
-int sum(int x, int y) // parameter
-{ 
-    
-     
-     return (x+y);
-    
-}
