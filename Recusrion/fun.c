0a1588cba0e2b62b66942b2556e9ef5307e0aab9
LOKESHKARMOKAR <146501836+LOKESHKARMOKAR@users.noreply.github.com>
2024-08-26 22:14:02 +0530

    33th

--- Recusrion/fun.c
+++ Recusrion/fun.c
@@ -37,3 +37,51 @@
 //     return 0;
 // }
 
+
+// *************** Indirect recursion
+
+// #include<stdio.h>
+// int facto1(int x){
+//     if(x==1){
+//         return 1;
+//     }else{
+//        return x*facto2(x-1);
+//     }
+// }
+// int facto2(int x){
+//     if(x==1){
+//         return 1;
+//     }else{
+//        return x*facto1(x-1);
+//     }
+// }
+// int main(){
+// int n, r;
+// printf("Enter the value of n : \n");
+// scanf("%d", &n);
+// r = facto1(n);
+// printf("The Factorial value of %d is %d", n, r);
+
+//     return 0;
+// }
+
+
+//  ****************Fibanocci Serics***********************
+
+#include<stdio.h>
+int fibo(int x){
+    if(x==0 || x==1){
+        return x;
+    }else{
+       return fibo(x-1)+fibo(x-2);
+    }
+}
+int main(){
+    int n,r;
+    printf("Enter the value : \n");
+    scanf("%d", &n);
+    r = fibo(n);
+    printf("The value of %d term is %d", n,r);
+    return 0;
+}
+
