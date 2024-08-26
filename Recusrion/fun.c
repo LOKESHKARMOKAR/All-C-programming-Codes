9161b1b75206ced9aa96849ace1995f8b6a15a17
LOKESHKARMOKAR <146501836+LOKESHKARMOKAR@users.noreply.github.com>
2024-08-26 22:14:29 +0530

    34th

--- Recusrion/fun.c
+++ Recusrion/fun.c
@@ -68,20 +68,22 @@
 
 //  ****************Fibanocci Serics***********************
 
-#include<stdio.h>
-int fibo(int x){
-    if(x==0 || x==1){
-        return x;
-    }else{
-       return fibo(x-1)+fibo(x-2);
-    }
-}
-int main(){
-    int n,r;
-    printf("Enter the value : \n");
-    scanf("%d", &n);
-    r = fibo(n);
-    printf("The value of %d term is %d", n,r);
-    return 0;
-}
+// #include<stdio.h>
+// int fibo(int x){
+//     if(x==0 || x==1){
+//         return x;
+//     }else{
+//        return fibo(x-1)+fibo(x-2);
+//     }
+// }
+// int main(){
+//     int n,r;
+//     printf("Enter the value : \n");
+//     scanf("%d", &n);
+//     r = fibo(n);
+//     printf("The value of %d term is %d", n,r);
+//     return 0;
+// }
+
+
 
