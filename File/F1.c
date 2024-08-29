b85d48aab92f42d820ccec0ee19c7071880c343a
LOKESHKARMOKAR <146501836+LOKESHKARMOKAR@users.noreply.github.com>
2024-08-29 23:45:11 +0530

    37th

--- File/F1.c
+++ File/F1.c
@@ -2,19 +2,40 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 int main()
 {
-    FILE *fp;
-    fp = fopen("C:/Users/lokes/OneDrive/Desktop/C Programming/chapter3/F2.txt","w");
+    // FILE *fp;
+    // char ch[100];
+    // fp = fopen("C:/Users/lokes/OneDrive/Desktop/C Programming/chapter3/F2.txt","a");
 
+    // if(fp==NULL){
+    //     printf("File does't exist\n");
+    //     exit(1);
+    // }else{
+    //     printf("Enter the text :\n");
+    //     gets(ch);
+    //     fputs(ch, fp);
+    //      fclose(fp);
+    // }
+
+   
+    // printf("File create successfull");
+
+    FILE *fp;
+    char ch;
+    fopen("File/bio.txt", "r");
     if(fp==NULL){
         printf("File does't exist\n");
         exit(1);
-    }else{
-
+    }
+    else{
+        ch = fgetc(fp);
+        printf("The charecter is %c", ch);
+        
     }
 
+    printf("\nFile create successfull");
     fclose(fp);
-    printf("File create successfull");
     return 0;
 }
